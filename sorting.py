e8a00984e0baf0f522770dbff80d8f85c4bf45da
Mikhail Smirnov <gear8mike@gmail.com>
2024-10-19 01:47:36 +0200

    buble sort for dev2

---
+++ sorting.py
@@ -0,0 +1,8 @@
+def bubble_sort(array):
+    pass
+
+if __name__ == '__main__':
+    array = [9, 5, 1, -8, 3, 7, 0, 25, 2]
+    print(f'Unsorted array: {array}')
+    bubble_sort(array)
+    print(f'Sorted array: {array}')
