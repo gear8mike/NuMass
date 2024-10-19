e4165258c81edab216f2c2bce94120bba7c05698
Mikhail Smirnov <gear8mike@gmail.com>
2024-10-19 15:32:05 +0200

    bubble sort algorithm added

--- sorting.py
+++ sorting.py
@@ -1,5 +1,12 @@
 def bubble_sort(array):
-    pass
+    n = len(array)
+    cond = True
+    while cond:
+        cond = False
+        for i in range(n-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                cond = True  # If a swap occurred, set condition to True to continue the loop
 
 if __name__ == '__main__':
     array = [9, 5, 1, -8, 3, 7, 0, 25, 2]
